abd15c68e4ea572ac09f31cdacac0b12baf4a146
Norv <norv@simplemachines.org>
2012-04-03 04:09:05 -0400

    Committing the Babylon theme files

---
+++ languages/Settings.english.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines
+ * @copyright 2011 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.0
+ */
+
+// Important! Before editing these language files please read the text at the topic of index.english.php.
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'The babylon theme from Simple Machines.<br /><br />Thanks go to Babylonking and Alienine.';
+
+?>
